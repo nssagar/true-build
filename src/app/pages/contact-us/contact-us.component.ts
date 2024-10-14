712f22a57034a7c7de8a631baa6d14cc456036b8
Suman Naryan <sumansagar@gmail.com>
2024-10-14 17:34:40 -0300

    Style updates

--- src/app/pages/contact-us/contact-us.component.ts
+++ src/app/pages/contact-us/contact-us.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent {
+export class ContactUsComponent implements OnInit {
+
+  screenAtTop = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onScroll() {
+    if (window.scrollY > 250) {
+      this.screenAtTop = true;
+    } else {
+      this.screenAtTop = false;
+    }
+  }
+
+  ngOnInit(): void {
+    this.scrollToTop();
+  }
+
+  scrollToTop() {
+    window.scrollTo({ top: 0 });
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,7 +5,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
   screenAtTop = false;
 
